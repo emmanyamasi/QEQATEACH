232f6f17044c32c19144fa57ed1a6d10bcd27420
emmanyamasi <2004emmanyamasi@gmail.com>
2025-04-12 20:06:19 +0300

    skills

--- skills/backend/src/routes/skillsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getSkills } from '../controllers/skillsController'; // Import the controller function
-
-const router = express.Router();
-
-// Define route for fetching skills
-router.get('/skills', getSkills);
-
-export default router;
